1a7a5a0f467b64f2941f6cb85a1b62d707a0fd5e
thesis <riccardo.pazzi@mail.polimi.it>
2023-06-21 18:46:48 +0200

    Implemented dot product between encrypted matrices in Ciphertext.py

--- Ciphertext.py
+++ Ciphertext.py
@@ -10,10 +10,18 @@ BASE = 2
 
 class EncryptedMatrix:
     def __init__(self, from_array=None, parameters=None):
+        """
+        There's two different ways to create an EncryptedMatrix object: without any arguments, this will create an empty
+        object which can store an integer matrix using encrypts. With an array and parameters, this will simply copy the
+        passed array (which must already be encrypted) and store it alongside the parameters, creating a copy of the
+        passed array.
+        :param from_array:
+        :param parameters:
+        """
         if from_array is None:
             self.initialized = False # When false the matrix can encrypt an integer vector
             return
-        self.pk, self.size, self.q, self.t, self.poly_mod, self.std1 = parameters
+        self.pk, self.size, self.q, self.t, self.p, self.poly_mod, self.std1 = parameters
         self.packed_parameters = parameters
         self.shape = from_array.shape
         self.poly_matrix = from_array
@@ -21,12 +29,18 @@ class EncryptedMatrix:
 
     def encrypts(self, int_vector, parameters: tuple):
         """
-        Takes as input a matrix of integers and converts it into the corresponding polynomial representation
-        using binary base encoding
+        Takes as input a matrix of integers and converts it into the corresponding polynomial representation using
+        binary base encoding, then encrypts it with the public key provided in parameters
+        :param int_vector: The integer matrix to be encrypted
+        :param parameters: A tuple containing all necessary parameters for the
+        encryption and encoding steps. The tuple is formed as follows: (public_key, polynomial size, integer modulo
+        q, plaintext modulus t, scaling factor p, cyclotomic polynomial modulus, standard deviation for the error
+        polynomial generation)
+        :return: The encrypted version of the integer matrix
         """
         if self.initialized:
             print("WARNING! Overriding an encrypted array which was already stored into this Object")
-        self.pk, self.size, self.q, self.t, self.poly_mod, self.std1 = parameters
+        self.pk, self.size, self.q, self.t, self.p, self.poly_mod, self.std1 = parameters
         self.packed_parameters = parameters
         np_input = np.array(int_vector, dtype=np.int64)
         self.shape = np_input.shape + (2, self.size)
@@ -43,17 +57,72 @@ class EncryptedMatrix:
             # They might be shorter than the required dimension size, therefore we pad with zeros
             ct0 = np.pad(ct0, (0, self.size - len(ct0)), constant_values=0)
             ct1 = np.pad(ct1, (0, self.size - len(ct1)), constant_values=0)
-            if len(ct0) < self.size:
-                b = 1
             self.poly_matrix[idx + (0,)] += ct0
             self.poly_matrix[idx + (1,)] += ct1
 
         print(self.poly_matrix)
 
-    def __mul__(self, other):
+    def dot2D(self, other, rlk):
+        """
+        Dot product between two encrypted tensors, relinearization is automatically performed
+        :param other:
+        :param rlk: Relinearization key tuple (rlk0, rlk1)
+        :return: The dot product
+        """
         # We iterate through the entire matrix except the last two dimensions which store the polynomials
         # The shape will be calculated as usual for the matrix product A x B = C where A.shape = [3,4] B.shape = [4,1]
         # Therefore C.shape will be [3,1]
+        if len(self.shape) != 4:
+            raise NotImplementedError("dot2D only supports 2D matrix product, for batched product use batched_dot2D")
+        # Check the dimensions are matching
+        if not (self.shape[-3] == other.shape[0]):
+            raise ValueError(f"Matrix shapes {self.shape} and {other.shape} are not matching!")
+        # To compute output shape eliminate last three dimensions (last two are to store the pair of polynomials, the
+        # other dimension is the one which will be eliminated
+        poly_matrix_shape = self.shape[:-3] + other.shape[1:-2]  # Remove last three dimensions of
+        output_matrix = np.zeros(poly_matrix_shape + (2, self.size))
+        # Nested multiplication loop
+        # For multiplication the elements in the last dimension of the first tensor will be multiplied with the elements
+        # in the first dimension of the second tensor
+        for idx in np.ndindex(poly_matrix_shape):
+            result_poly = np.zeros((2, self.size), dtype=np.int64)
+            for index in range(self.shape[-3]):
+                # Compute the indexes which will range to compute dot product between 1D slices of the tensor
+                first_idx = idx[:-1] + (index,)
+                second_idx = (index,) + idx[1:]
+                # Extract first element which will be multiplied
+                ct0_0 = self.poly_matrix[first_idx + (0,)]
+                ct1_0 = self.poly_matrix[first_idx + (1,)]
+
+                # Extract second polynomial of the multiplication
+                ct0_1 = other.poly_matrix[second_idx + (0,)]
+                ct1_1 = other.poly_matrix[second_idx + (1,)]
+
+                # Multiply encrypted polynomials
+                mul_poly = mul_cipher_v2((ct0_0, ct1_0), (ct0_1, ct1_1), self.q, self.t, self.p, self.poly_mod, rlk[0], rlk[1])
+
+                # Sum all the mul poly into one polynomial
+                result_poly = add_cipher(result_poly, mul_poly, self.q, self.poly_mod)
+
+            # Extract the two polynomials in the resulting ciphertext
+            ct0 = result_poly[0]
+            ct1 = result_poly[1]
+
+            # Pad with 0 results to always match the complete size of the polynomials
+            ct0 = np.pad(ct0, (0, self.size - len(ct0)), constant_values=0)
+            ct1 = np.pad(ct1, (0, self.size - len(ct1)), constant_values=0)
+            # Update output_matrix accordingly
+            output_matrix[idx + (0,)] += ct0
+            output_matrix[idx + (1,)] += ct1
+
+        # Return EncryptedMatrix object containing the output matrix
+        e_result = EncryptedMatrix(output_matrix, self.packed_parameters)
+        return e_result
+
+    def __rmul__(self, other):
+        if not (isinstance(other, int) or isinstance(other, np.int64)):
+            print("ERROR! Cannot use * operator with elements other than integers")
+            return self
         return self
 
     def __add__(self, other):
@@ -95,11 +164,11 @@ if __name__ == "__main__":
     # Running this module will run tests
     # Scheme's params
     # polynomial modulus degree, SIZE parameter
-    n = 2 ** 2
+    n = 2 ** 4
     # ciphertext modulus, MODULUS parameter
-    q = 2 ** 14
+    q = 2 ** 15
     # plaintext modulus
-    t = 8
+    t = 4
     # base for relin_v1
     T = int(np.sqrt(q))
     # modulusswitching modulus
@@ -108,10 +177,12 @@ if __name__ == "__main__":
     poly_mod = np.array([1] + [0] * (n - 1) + [1])
     # Std for encryption
     std1 = 1
+    std2 = 1
     # generate public and secret keys
     pk, sk = keygen(n, q, poly_mod, std1)
+    rlk = evaluate_keygen_v2(sk, n, q, poly_mod, p, std2)
 
-    params = (pk, n, q, t, poly_mod, std1)
+    params = (pk, n, q, t, p, poly_mod, std1)
 
     int_matrix = [[1, 2], [3, 4]]
     int_matrix2 = [[1, 2], [3, 4]]
@@ -136,4 +207,9 @@ if __name__ == "__main__":
 
     print("CHECK SUM FUNCTION:")
     decrypted_matrix = c_3.decrypt(sk)
+    print(decrypted_matrix)
+
+    print("DOT PRODUCT CHECK:")
+    c_4 = c_1.dot2D(c_2, rlk)
+    decrypted_matrix = c_4.decrypt(sk)
     print(decrypted_matrix)
