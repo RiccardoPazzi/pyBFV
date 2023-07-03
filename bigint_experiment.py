Fixed issues with multiplicative depth testing and big integer operations where wrong results were produced for q > 2^32

bigint_experiment.py
@@ -4,11 +4,11 @@ from pyBFV.BFV_bigint import *
 if __name__ == '__main__':
     # Scheme's params
     # polynomial modulus degree, SIZE parameter
-    n = 2 ** 4
+    n = 2 ** 5
     # ciphertext modulus, MODULUS parameter
-    q = 2 ** 33
+    q = 2 ** 60
     # plaintext modulus
-    t = 8
+    t = 2 ** 3
 
     # modulusswitching modulus
     p = q ** 3
@@ -17,9 +17,9 @@ if __name__ == '__main__':
     poly_mod = np.array([1] + [0] * (n - 1) + [1])
 
     # standard deviation for the error in the encryption, common value 3.2
-    std1 = 0
+    std1 = 1
     # standard deviation for the error in the evaluateKeyGen_v2
-    std2 = 0
+    std2 = 1
 
     # Multiparty procedures with 2 parties
 
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     print(rlk0, rlk1)
 
     # PARTY 1 message
-    m1 = int2base(17, 2)
+    m1 = int2base(127, 2)
     # PARTY 2 message
-    m2 = int2base(2, 2)
+    m2 = int2base(33, 2)
     print(m1, m2)
 
     ct1 = encrypt(pk, n, q, t, poly_mod, m1, std1)
@@ -45,6 +45,8 @@ if __name__ == '__main__':
 
     # Multiplication experiment
     e_mult = mul_cipher_v2(ct1, ct2, q, t, p, poly_mod, rlk0, rlk1)
+    e_mult = mul_cipher_v2(e_mult, ct2, q, t, p, poly_mod, rlk0, rlk1)
+    e_mult = mul_cipher_v2(e_mult, ct2, q, t, p, poly_mod, rlk0, rlk1)
     d_mult = decrypt(sk, q, t, poly_mod, e_mult)
     e_sum = add_cipher(ct1, ct2, q, poly_mod)
     e_sum = add_cipher(e_sum, ct1, q, poly_mod)
@@ -52,5 +54,5 @@ if __name__ == '__main__':
     d_sum = decrypt(sk, q, t, poly_mod, e_sum)
 
     # Multiplication result
-    print(d_mult)
+    print(base2int(d_mult, 2))
     print(d_sum)

pyBFV/BFV_bigint.py
@@ -46,8 +46,7 @@ def polymul(x, y, modulus, poly_mod):
     Returns:
         A polynomial in Z_modulus[X]/(poly_mod).
     """
-    return bigint.polydiv(bigint.polymul(x, y) %
-                          modulus, poly_mod)[1] % modulus
+    return bigint.polydiv(bigint.polymul(x, y), poly_mod)[1] % modulus
 
 
 def polyadd(x, y, modulus, poly_mod):
@@ -59,8 +58,7 @@ def polyadd(x, y, modulus, poly_mod):
     Returns:
         A polynomial in Z_modulus[X]/(poly_mod).
     """
-    return bigint.polydiv(poly.polyadd(x, y) %
-                          modulus, poly_mod)[1] % modulus
+    return bigint.polydiv(poly.polyadd(x, y), poly_mod)[1] % modulus
 
 
 # ==============================================================
@@ -75,7 +73,7 @@ def gen_binary_poly(size):
         array of coefficients with the coeff[i] being
         the coeff of x ^ i.
     """
-    return np.random.randint(-1, 2, size, dtype=np.int64)
+    return np.array(np.random.randint(-1, 2, size), dtype=object)
 
 
 def gen_uniform_poly(size, modulus):
@@ -100,7 +98,7 @@ def gen_normal_poly(size, mean, std):
         array of coefficients with the coeff[i] being
         the coeff of x ^ i.
     """
-    return np.int64(np.random.normal(mean, std, size=size))
+    return np.array(np.int64(np.random.normal(mean, std, size=size)), dtype=object)
 
 
 # ==============================================================
@@ -189,12 +187,10 @@ def encrypt(pk, size, q, t, poly_mod, m, std1):
     e1 = gen_normal_poly(size, 0, std1)
     e2 = gen_normal_poly(size, 0, std1)
     u = gen_binary_poly(size)
+    prod = polymul(pk[0], u, q, poly_mod)
     ct0 = polyadd(
-        polyadd(
-            polymul(pk[0], u, q, poly_mod),
-            e1, q, poly_mod),
-        scaled_m, q, poly_mod
-    )
+        polyadd(prod, e1, q, poly_mod),
+        scaled_m, q, poly_mod)
     ct1 = polyadd(
         polymul(pk[1], u, q, poly_mod),
         e2, q, poly_mod

test/test_MultiplicativeDepth.py
@@ -1,10 +1,10 @@
 import unittest
 import numpy as np
-from pyBFV.BFV import *
+from pyBFV.BFV_bigint import *
 
 
-def generate_int(in_range: tuple):
-    return np.random.randint(in_range[0], in_range[1], dtype=np.int64)
+def generate_int(in_range: tuple, size=1):
+    return np.array(np.random.randint(in_range[0], in_range[1], size=size, dtype=np.int64), dtype=object)
 
 
 class TestCase(unittest.TestCase):
@@ -12,29 +12,25 @@ class TestCase(unittest.TestCase):
         # Setting up the scheme
         # PARAMETERS MUST ADHERE TO THE FORMULA HERE:
         # https://bit-ml.github.io/blog/post/homomorphic-encryption-toy-implementation-in-python/
-        self.desired_range = (2, 4)
+        self.desired_range = (2, 10)
+        self.max_depth = 15
+        # Scheme's params
         # polynomial modulus degree, SIZE parameter
-        # Limits the max number representable in base 2 (2 ^ n - 1)
-        self.n = 2 ** 3
-        # 2 ** 6 doesn't work. Max parameters are 2 ** 5, q = 2^27, t = 64
-        # print(f"BASE decomposition will work up to {2 ** self.n}")
-
+        self.n = 2 ** 6
         # ciphertext modulus, MODULUS parameter
-        self.q = 2 ** 29
-
+        self.q = 2 ** 500
         # plaintext modulus
-        # Some operations might still produce the expected results when the number exceeds t due to base decomposition
-        # However t for sure limits the number of sums to t, because even with binary decomposition the 1's added will
-        # eventually surpass e.g. 128
-        # Limiting the number of sums to t will in turn limit the dot product which can only operate on tensors of the form
-        # (.. x t) . (t x ..) where t is the dimension which will be eliminated from both tensors
-        self.t = 4
-        # modulus switching modulus
-        self.p = self.q
-        # Polynomial modulus
+        self.t = 2 ** 10
+
+        # modulusswitching modulus
+        self.p = self.q ** 3
+
+        # polynomial modulus
         self.poly_mod = np.array([1] + [0] * (self.n - 1) + [1])
-        # Std for encryption
+
+        # standard deviation for the error in the encryption, common value 3.2
         self.std1 = 1
+        # standard deviation for the error in the evaluateKeyGen_v2
         self.std2 = 1
         # Base for decomposition
         self.BASE = 2
@@ -45,71 +41,57 @@ class TestCase(unittest.TestCase):
         estimate the multiplicative capabilities of the selected parameters
         :return:
         """
-        # generate public, secret and relinearization keys
-        pk, sk = keygen(self.n, self.q, self.poly_mod, self.std1)
-        rlk = evaluate_keygen_v2(sk, self.n, self.q, self.poly_mod, self.p, self.std2)
-
-        params = (pk, self.n, self.q, self.t, self.p, self.poly_mod, self.std1)
-        correct_result = True
-        mul_depth = 1
-        expected_result = generate_int(self.desired_range)
-        e_m1 = int2base(expected_result, self.BASE)
-        ctx1 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, e_m1, self.std1)
-        while correct_result:
-            m1 = expected_result
-            m2 = generate_int(self.desired_range)
-            expected_result = m1 * m2
-            # PARTY 2 message
-            e_m2 = int2base(m2, self.BASE)
-            ctx2 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, e_m2, self.std1)
-            # Multiplication experiment
-            ctx1 = mul_cipher_v2(ctx1, ctx2, self.q, self.t, self.p, self.poly_mod, rlk[0], rlk[1])
-            d_mult = decrypt(sk, self.q, self.t, self.poly_mod, ctx1)
-            # Decode result
-            actual_result = base2int(d_mult, self.BASE)
-            print(f"MULTIPLICATIVE DEPTH: {mul_depth}")
-            print(f"Expected result: {expected_result}, actual result: {actual_result}")
-            if expected_result > (2 ** self.n):
-                print(f"Result is over the bit capacity!")
-            self.assertTrue(actual_result == expected_result)
-            mul_depth += 1
-
-    def test_addition_depth(self):
-        """
-        Selects two integers at random and continues adding until the result is incorrect, this can be used to
-        estimate the additive capabilities of the selected parameters
-        :return:
-        """
-        # generate public and secret keys
         pk, sk = keygen(self.n, self.q, self.poly_mod, self.std1)
 
-        params = (pk, self.n, self.q, self.t, self.p, self.poly_mod, self.std1)
-        correct_result = False
-        sum_depth = 1
-        expected_result = generate_int(self.desired_range)
-        while correct_result:
-            m1 = expected_result
-            m2 = generate_int(self.desired_range)
-            expected_result = m1 + m2
-            # PARTY 1 message
-            e_m1 = int2base(m1, self.BASE)
-            # PARTY 2 message
-            e_m2 = int2base(m2, self.BASE)
-            # Encryption
-            ctx1 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, e_m1, self.std1)
-            ctx2 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, e_m2, self.std1)
-            # Multiplication experiment
-            e_sum = add_cipher(ctx1, ctx2, self.q, self.poly_mod)
-            d_sum = decrypt(sk, self.q, self.t, self.poly_mod, e_sum)
-            # Decode result
-            actual_result = base2int(d_sum, self.BASE)
-            if sum_depth % 50 == 0:
-                print(f"MULTIPLICATIVE DEPTH: {sum_depth}")
-                print(f"Expected result: {expected_result}, actual result: {actual_result}")
-                if expected_result > (2 ** self.n):
-                    print(f"Result is over the bit capacity!")
-            self.assertTrue(actual_result == expected_result)
-            sum_depth += 1
+        # EvaluateKeygen_version2
+        rlk0, rlk1 = evaluate_keygen_v2(sk, self.n, self.q, self.poly_mod, self.p, self.std2)
+
+        # PARTY 1 message
+        m1 = int2base(127, 2)
+        # PARTY 2 message
+        m2 = int2base(33, 2)
+
+        ct1 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, m1, self.std1)
+        ct2 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, m2, self.std1)
+
+        # Multiplication experiment
+        e_mult = mul_cipher_v2(ct1, ct2, self.q, self.t, self.p, self.poly_mod, rlk0, rlk1)
+        e_mult = mul_cipher_v2(e_mult, ct2, self.q, self.t, self.p, self.poly_mod, rlk0, rlk1)
+        e_mult = mul_cipher_v2(e_mult, ct2, self.q, self.t, self.p, self.poly_mod, rlk0, rlk1)
+        d_mult = decrypt(sk, self.q, self.t, self.poly_mod, e_mult)
+
+        # Multiplication result
+        print(base2int(d_mult, 2))
+
+        # Iterate through random integer list
+        rand_list = generate_int(self.desired_range, self.max_depth)
+        expected_result = rand_list[0]
+        m1 = int2base(expected_result, 2)
+        ct1 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, m1, self.std1)
+        depth_reached = 1
+
+        for num in rand_list[1:]:
+            # Updating expected result in plaintext
+            old_expected_r = expected_result
+            expected_result = expected_result * num
+
+            # Computing encoding and encryption for second term of the multiplication
+            m2 = int2base(num, 2)
+            ct2 = encrypt(pk, self.n, self.q, self.t, self.poly_mod, m2, self.std1)
+
+            # Encrypted multiplication
+            e_mult = mul_cipher_v2(ct1, ct2, self.q, self.t, self.p, self.poly_mod, rlk0, rlk1)
+            # Reusing ct1 to contain multiplication result
+            ct1 = e_mult
+
+            # Decrypting and checking multiplication
+            d_mult = decrypt(sk, self.q, self.t, self.poly_mod, e_mult)
+            d_result = base2int(d_mult, 2)
+            print(f"CURRENT DEPTH: {depth_reached}")
+            print(f"EXPECTED RESULT: {old_expected_r}*{num}={expected_result}. ENCRYPTED RESULT: {d_result}")
+            # Increase depth
+            depth_reached += 1
+
 
 
 if __name__ == '__main__':